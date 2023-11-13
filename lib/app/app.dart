7812ab2726419ba269def71dc62cf3c29315c8a8
maykhid <henryifebunandu@gmail.com>
2023-11-13 19:06:44 +0100

    update: auth screen

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:eden_task/app/features/auth/ui/auth_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class EdenApp extends StatelessWidget {
   const EdenApp({super.key});
@@ -7,8 +8,14 @@ class EdenApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme(textTheme).copyWith(
+          bodySmall: GoogleFonts.oswald(textStyle: textTheme.bodySmall),
+        ),
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: false,
       ),
--- lib/app/features/auth/ui/auth_screen.dart
+++ lib/app/features/auth/ui/auth_screen.dart
@@ -1,20 +1,59 @@
-
+import 'package:eden_task/app/shared/ui/app_button.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class AuthScreen extends StatefulWidget {
-  const AuthScreen({
-    super.key,
-  });
+class AuthScreen extends StatelessWidget {
+  const AuthScreen({super.key});
 
-  @override
-  State<AuthScreen> createState() => _AuthScreenState();
-}
-
-class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Eden',
+                style: GoogleFonts.playfairDisplay(
+                  fontSize: 40,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.green.shade900,
+                ),
+              ),
+              const Gap(8),
+              FaIcon(
+                FontAwesomeIcons.leaf,
+                size: 60,
+                color: Colors.green.shade900,
+              ),
+            ],
+          ),
+          const Gap(80),
+          const Text('Continue with'),
+          const Gap(20),
+          AppIconButton(
+            text: 'Sign in with Google',
+            icon: const FaIcon(FontAwesomeIcons.google, size: 24),
+            onPressed: () {},
+          ),
+          const SizedBox(height: 10),
+
+           const Text('or'),
+
+           const SizedBox(height: 10),
+
+          AppIconButton(
+            text: 'Sign in with GitHub',
+            icon: const FaIcon(FontAwesomeIcons.github, size: 24),
+            onPressed: () {},
+            color: Colors.black,
+          ),
+        ],
+      ),
     );
   }
 }
