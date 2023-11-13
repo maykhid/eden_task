f5ce7989128ed994780894502b6046c551c85424
maykhid <henryifebunandu@gmail.com>
2023-11-13 17:35:45 +0100

    minor app setup and refactors

--- lib/core/data/api_config.dart
+++ lib/core/data/api_config.dart
@@ -1,5 +1,7 @@
-const String ablyApiKeyRef = "ablyApiKey";
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+const String ablyApiKeyRef = 'ablyApiKey';
 
 class ApiConfig {
-
+  static String get ablyApiKey => dotenv.env[ablyApiKeyRef] ?? '';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,17 @@
 import 'package:eden_task/app/app.dart';
+import 'package:eden_task/core/di/di.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  setup();
   runApp(const EdenApp());
 }
 
-
-
-class AuthScreen extends StatefulWidget {
-  const AuthScreen({
-    super.key,
-  });
-
-  @override
-  State<AuthScreen> createState() => _AuthScreenState();
+// ignore:  inference_failure_on_function_return_type, always_declare_return_types, type_annotate_public_apis
+setup() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  initDependencies();
+  await dotenv.load();
 }
 
-class _AuthScreenState extends State<AuthScreen> {
-
-
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(),
-    );
-  }
-}
